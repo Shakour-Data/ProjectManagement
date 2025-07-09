7e7d75e225df0ed94d0fbaac7c3a3d254e25360a
GravityMg <164815641+GravityMg@users.noreply.github.com>
2025-07-09 18:04:54 +0000

    🐛 fix(Project_Management): Modified files updated\n\nChanges included (as of 2025-07-09 18:04:54):\n- Project_Management/modules/dashboards_reports.py: This file was modified with updates or fixes.\n  Summary:\n    diff --git a/Project_Management/modules/dashboards_reports.py b/Project_Management/modules/dashboards_reports.py\n    index 66d2e35..8d10eb7 100644\n    --- a/Project_Management/modules/dashboards_reports.py\n    +++ b/Project_Management/modules/dashboards_reports.py\n    @@ -3,7 +3,7 @@ from typing import Dict, Any, List, Optional\n\n\nPlease describe the reason or issue addressed by these changes.

--- Project_Management/modules/dashboards_reports.py
+++ Project_Management/modules/dashboards_reports.py
@@ -3,7 +3,7 @@ from typing import Dict, Any, List, Optional
 from Project_Management.modules.progress_calculator import ProgressCalculator
 
 class DashboardReports:
-    def __init__(self, input_dir: str = 'PM_Input'):
+    def __init__(self, input_dir: str = 'PM_JSON/user_inputs'):
         self.input_dir = input_dir
         self.data = {}
         self.progress_calculator = ProgressCalculator(input_dir)
