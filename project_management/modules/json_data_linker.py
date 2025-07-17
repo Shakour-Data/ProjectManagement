05ef3f0085cae3e434dba87c989f7872bd1b368e
GravityWavesFundamental <164954320+GravityWavesFundamental@users.noreply.github.com>
2025-07-17 20:31:48 +0000

    🐛 fix(project_management): Modified files updated\n\nChanges included (as of 2025-07-17 20:31:48):\n- project_management/modules/json_data_linker.py: This file was modified with updates or fixes.\n  Summary:\n    diff --git a/project_management/modules/json_data_linker.py b/project_management/modules/json_data_linker.py\n    index bcdb3cf0..8035c99b 100644\n    --- a/project_management/modules/json_data_linker.py\n    +++ b/project_management/modules/json_data_linker.py\n    @@ -2,7 +2,7 @@ import json\n\n\nPlease describe the reason or issue addressed by these changes.

--- project_management/modules/json_data_linker.py
+++ project_management/modules/json_data_linker.py
@@ -2,7 +2,7 @@ import json
 import os
 
 class JSONDataLinker:
-    def __init__(self, input_dir="project_inputs/PM_JSON/user_inputs", intermediate_dir="project_inputs/PM_JSON/intermediate", output_dir="project_inputs/PM_JSON/system_outputs"):
+    def __init__(self, input_dir="project_management/PM_Input", intermediate_dir="project_management/PM_JSON/intermediate", output_dir="project_management/PM_JSON/system_outputs"):
         self.input_dir = input_dir
         self.intermediate_dir = intermediate_dir
         self.output_dir = output_dir
