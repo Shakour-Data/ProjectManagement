7a78fc5363311fec52e8dbd1d9226cf9e97d1c55
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-07-29 21:03:46 +0330

    🐛 fix(build_installer_scripts): Modified files updated\n\nChanges included (as of 2025-07-29 21:03:46):\n- build_installer_scripts/build_installer_gui.sh: This file was modified with updates or fixes.\n  Summary:\n    diff --git a/build_installer_scripts/build_installer_gui.sh b/build_installer_scripts/build_installer_gui.sh\n    index 3d90a3d3..92f49085 100755\n    --- a/build_installer_scripts/build_installer_gui.sh\n    +++ b/build_installer_scripts/build_installer_gui.sh\n    @@ -11,7 +11,7 @@ mkdir -p $DIST_DIR\n\n\nPlease describe the reason or issue addressed by these changes.

--- build_installer_scripts/build_installer_gui.sh
+++ build_installer_scripts/build_installer_gui.sh
@@ -11,7 +11,7 @@ mkdir -p $DIST_DIR
 
 echo "Building graphical installer executable for Linux using Flask-based UI..."
 source $VENV_PATH/bin/activate
-pyinstaller --onefile --name ProjectManagementInstaller_Linux $SCRIPT_PATH
+pyinstaller --onefile --name ProjectManagementInstaller_Linux --add-data "installer_gui/ui:ui" $SCRIPT_PATH
 mv dist/ProjectManagementInstaller_Linux $DIST_DIR/
 
 echo "Build complete. Executable is in $DIST_DIR"
