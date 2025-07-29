2978c99ece68f887f506ff42c872b03f91ee9295
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-07-29 18:07:42 +0330

    🐛 fix(build_installer_scripts): Modified files updated\n\nChanges included (as of 2025-07-29 18:07:42):\n- build_installer_scripts/build_installer_gui.sh: This file was modified with updates or fixes.\n  Summary:\n    diff --git a/build_installer_scripts/build_installer_gui.sh b/build_installer_scripts/build_installer_gui.sh\n    index 14b2a93d..1e59a50e 100755\n    --- a/build_installer_scripts/build_installer_gui.sh\n    +++ b/build_installer_scripts/build_installer_gui.sh\n    @@ -9,7 +9,7 @@ DIST_DIR="./dist_installers"\n\n\nPlease describe the reason or issue addressed by these changes.

--- build_installer_scripts/build_installer_gui.sh
+++ build_installer_scripts/build_installer_gui.sh
@@ -9,7 +9,7 @@ DIST_DIR="./dist_installers"
 mkdir -p $DIST_DIR
 
 echo "Building graphical installer executable for Linux..."
-pyinstaller --onefile --name ProjectManagementInstaller_Linux $SCRIPT_PATH
+pyinstaller --onefile --name ProjectManagementInstaller_Linux --hidden-import=PyQt5.QtWebEngineWidgets --hidden-import=PyQtWebEngineWidgets $SCRIPT_PATH
 mv dist/ProjectManagementInstaller_Linux $DIST_DIR/
 
 echo "Build complete. Executable is in $DIST_DIR"
