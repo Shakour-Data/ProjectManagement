c2d6ac9e17edacdab80aa1c59d195ae7f7fb51f0
GravityWavesPr <GravityWavesProgramming@gmail.com>
2025-07-31 17:26:47 +0330

    🐛 fix(project_management): Modified files updated\n\nChanges included (as of 2025-07-31 17:26:47):\n- project_management/modules/__init__.py: This file was modified with updates or fixes.\n  Summary:\n    diff --git a/project_management/modules/__init__.py b/project_management/modules/__init__.py\n    index 4c606eac..91df3c4d 100644\n    --- a/project_management/modules/__init__.py\n    +++ b/project_management/modules/__init__.py\n    @@ -1 +1,4 @@\n\n\nPlease describe the reason or issue addressed by these changes.

--- project_management/modules/__init__.py
+++ project_management/modules/__init__.py
@@ -1,1 +1,4 @@
-# project_management.modules package initialization
+# This __init__.py file is used to make the modules directory a package.
+# It can be used to import submodules or subpackages for easier access.
+
+# Import subpackages for services and project_management modules
